c33973b5780637b7ff02c4b1fb5b010000a3d7d3
Celia Lobet <lobet.c@gmail.com>
2021-05-31 15:07:39 +0200

    2nd commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Feature 1-a
+
+// const clickFooter = document.getElementsByTagName("footer")[0];
+// clickFooter.addEventListener("click", function () {
+//   console.log("click");
+// });
+
+// Feature 1-b
+
+let count = 0;
+const clickFooter = document.getElementsByTagName("footer")[0];
+clickFooter.addEventListener("click", function () {
+  count++;
+  console.log(`clic numéro ${count}`);
+});
+
+// Feature 2
+let menu = document.querySelector("#navbarHeader");
+let menuBtn = document.querySelector(".navbar-toggler");
+
+menuBtn.addEventListener("click", function () {
+  menu.classList.toggle("collapse");
+});
+
+// Feature 3
+let firstCard = document.querySelectorAll(".card")[0];
+let firstEditBtn = document.querySelectorAll(".btn-outline-secondary")[0];
+firstEditBtn.addEventListener("click", function () {
+  firstCard.style = "color:red";
+});
+
+// Feature 4
+let secondCard = document.querySelectorAll(".card")[1];
+let secondEditBtn = document.querySelectorAll(".btn-outline-secondary")[1];
+secondEditBtn.addEventListener("click", function () {
+  if (secondCard.style.color == "green") {
+    secondCard.style.color = "";
+  } else {
+    secondCard.style.color = "green";
+  }
+});
